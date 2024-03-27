055b096d1ad7f4f7640691a507f273512e66b124
Oleksandr <zagrebelniysasha@gmail.com>
2024-03-27 12:56:59 +0200

    feat: events csv file

---
+++ scripts/events.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+from config import INPUT_CSV_DIR, OUTPUT_CSV_DIR
+
+races_path = os.path.join(INPUT_CSV_DIR, 'races.csv')
+seasons_path = os.path.join(OUTPUT_CSV_DIR, 'seasons.csv')
+races = pd.read_csv(races_path)
+seasons = pd.read_csv(seasons_path)
+
+year_season_map = dict(zip(seasons['year'], seasons['seasonId']))
+
+races['seasonId'] = races['year'].map(year_season_map)
+
+events = races.drop('year', axis=1)
+
+events.to_csv(os.path.join(OUTPUT_CSV_DIR, 'events.csv'), index=False)
